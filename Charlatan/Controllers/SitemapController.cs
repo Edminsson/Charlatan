9ee69e321e0664c2cb7f60a987654605d5e4a15b
Axel Aguilar <axel.aguilar@gmail.com>
2015-08-06 17:55:21 +0200

    Ändrar i användningen av MemoryStream så att det disposas på rätt sätt

--- Charlatan/Controllers/SitemapController.cs
+++ Charlatan/Controllers/SitemapController.cs
@@ -7,6 +7,7 @@ using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Web.Http;
+using System.Xml;
 using System.Xml.Serialization;
 
 namespace Charlatan.Controllers
@@ -27,10 +28,24 @@ namespace Charlatan.Controllers
             XmlSerializerNamespaces ns = new XmlSerializerNamespaces();
             ns.Add(string.Empty, "http://www.sitemaps.org/schemas/sitemap/0.9");
 
-            MemoryStream ms = new MemoryStream();
-            srlr.Serialize(ms, sajtkarta, ns);
+            //MemoryStream ms = new MemoryStream();
+            //srlr.Serialize(ms, sajtkarta, ns);
 
-            return Encoding.UTF8.GetString(ms.ToArray());
+            string serialiseradSajtkarta;
+
+
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                using (XmlWriter xmlWriter = XmlWriter.Create(memoryStream))
+                {
+                    srlr.Serialize(xmlWriter, sajtkarta, ns);
+                }
+
+                serialiseradSajtkarta = Encoding.UTF8.GetString(memoryStream.ToArray());
+
+            }
+
+            return serialiseradSajtkarta;
         }
 
     }
